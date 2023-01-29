a4149f245f3c6de634d3c3fceebbb18b21b7b4d2
Shubham-sharma12345 <shubhamsharma6641@gmail.com>
2023-01-29 19:40:55 +0530

    changes

---
+++ Arrays/kthsmallestele.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+class Solution{
+    public:
+   
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        priority_queue<int> maxHeap;
+        for(int i=l;i<=r;i++){
+            maxHeap.push(arr[i]);
+            if(maxHeap.size() > k){
+                maxHeap.pop();
+            }
+          
+        }
+        return maxHeap.top();
+    }    
+};
